db8c05db073a777e8dc9db292d7e05b236886f14
Nana Vanina <vaninalarissa@gmail.com>
2023-08-15 15:50:09 +0100

    Fix routing errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  authenticated :user do
+    root to: "categories#index", as: :authenticated_root
+  end
+
+  root to: 'home#splash'
+
+  resources :categories, only: [:index, :new, :create, :edit, :update, :destroy] do
+    resources :expenditures, only: [:index, :new, :create, :edit, :update, :destroy]
+
+  end
 end
